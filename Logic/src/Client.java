ed4b47d3d97b34be9e8d94ab2d2977437a8f49e5
roiSela <rugh5678@gmail.com>
2022-03-19 01:03:45 +0200

    final update
    just built the transaction class and did some work on section 2 and the other classes

---
+++ Logic/src/Client.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+public class Client {
+    private String clientName;
+    private double currentBalance;
+    private List<Transaction> clientTransactions;
+    private List<String> clientLoanIds;
+
+}
+
