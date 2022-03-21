8354d4fe9b9ac130658390e395f98c43a52c8027
roiSela <rugh5678@gmail.com>
2022-03-21 23:50:21 +0200

    doing what aviad said

--- Logic/src/Account.java
+++ Logic/src/Account.java
@@ -3,4 +3,12 @@ import java.util.*;
 public class Account {
     private List<Transaction> clientTransactions;
     private double currentBalance;
+
+
+    public List<Transaction> getClientTransactions() {
+        return clientTransactions;
+    }
+    public double getCurrentBalance() {
+        return currentBalance;
+    }
 }
